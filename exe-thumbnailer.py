f5e2890efe7ed8629cc31e29c773faafe6b910a9
James Lu <james@overdrivenetworks.com>
2019-10-23 08:20:14 -0700

    Always write a "normal" sized thumbnail
    For programs providing a 256x256 icon, downscale it to 128x128. Otherwise, let PIL choose the best icon size.

--- exe-thumbnailer.py
+++ exe-thumbnailer.py
@@ -15,7 +15,6 @@ import os
 from gi.repository import GLib
 
 from PIL import Image
-from PIL.IcoImagePlugin import IcoFile
 
 def main(inputfile, uri):
     print("Got args:", sys.argv)
@@ -23,24 +22,30 @@ def main(inputfile, uri):
     basedir = pathlib.Path(GLib.get_user_cache_dir()) / "thumbnails"
 
     im = None
-    try:
-        extractor = icoextract.IconExtractor(inputfile)
-        data = extractor.get_icon()
 
-        im = Image.open(data)  # Open up the .ico from memory
-        if (256, 256) in im.info['sizes']:
-            outdir = basedir / "large"
-        else:
-            outdir = basedir / "normal"
-    except:
-        traceback.print_exc()
-        outdir = basedir / "fail"
+    extractor = icoextract.IconExtractor(inputfile)
+    data = extractor.get_icon()
 
+    im = Image.open(data)  # Open up the .ico from memory
+    # Write a large icon if available
+    if (256, 256) in im.info['sizes']:
+        outdir = basedir / "large"
+        os.makedirs(outdir, mode=0o700, exist_ok=True)
+        outfile = outdir / (thumb_hash + '.png')
+        print("Writing large size thumbnail to", outfile)
+        im.save(outfile)
+
+        # But also write a normal size icon in all cases - downscale to 128x128 per
+        # https://specifications.freedesktop.org/thumbnail-spec/thumbnail-spec-latest.html
+        # "Normal: The default place for storing thumbnails. The image size must not exceed 128x128 pixel."
+        # Only some file managers (e.g. Caja) will show a thumbnail if only the large one exists?
+        im.resize((128, 128))
+
+    outdir = basedir / "normal"
     os.makedirs(outdir, mode=0o700, exist_ok=True)
     outfile = outdir / (thumb_hash + '.png')
-    print("Writing thumbnail to", outfile)
-    if im:
-        im.save(outfile)   # Convert it to a PNG
+    print("Writing normal size thumbnail to", outfile)
+    im.save(outfile)   # Convert it to a PNG
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__.splitlines()[0])
