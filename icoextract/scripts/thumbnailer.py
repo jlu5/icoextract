3d1fc43558053e0dc9cd48ddeca3f079db70b23a
James Lu <james@overdrivenetworks.com>
2026-05-24 19:07:38 -0700

    exe-thumbnailer: simplify branching

--- icoextract/scripts/thumbnailer.py
+++ icoextract/scripts/thumbnailer.py
@@ -40,23 +40,17 @@ def generate_thumbnail(inputfile, outfile, size=256, force_resize=False):
             logger.warning('Unsupported size %d, falling back to 128x128', size)
             size = 128
 
-        # Note: 256x256 is the largest size supported by the .ico format
-        if size == 256:
-            # A large size thumbnail was requested. No downwards resizing is needed, so export any icon as is
-            logger.debug("Writing large size thumbnail for %s to %s", inputfile, outfile)
-            im.save(outfile, "PNG")
-            return
-
-        # If large size thumbnail wasn't requested but one is available, pick an 128x128 icon if available;
-        # otherwise scale down from 256x256 to 128x128. 128x128 is the largest resolution allowed for
-        # "normal" size thumbnails.
-        if (128, 128) in im.info['sizes']:
-            logger.debug("Using native 128x128 icon")
-            im.size = (128, 128)
-        elif im.size > (128, 128):
-            logger.debug("Downsizing icon to 128x128")
-            im = im.resize((128, 128))
-        logger.debug("Writing normal size thumbnail for %s to %s", inputfile, outfile)
+        if size == 128:
+            # If large size thumbnail wasn't requested but one is available, pick an 128x128 icon if available;
+            # otherwise scale down from 256x256 to 128x128. 128x128 is the largest resolution allowed for
+            # "normal" size thumbnails.
+            if (128, 128) in im.info['sizes']:
+                logger.debug("Using native 128x128 icon")
+                im.size = (128, 128)
+            elif im.size > (128, 128):
+                logger.debug("Downsizing icon to 128x128")
+                im = im.resize((128, 128))
+        logger.debug("Writing size %s thumbnail for %s to %s", size, inputfile, outfile)
 
     im.save(outfile, "PNG")
 
