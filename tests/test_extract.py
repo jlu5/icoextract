8a2256b4c4b02330742d77eab7f4e9c372dadaf1
James Lu <james@overdrivenetworks.com>
2026-05-24 23:20:06 -0700

    Add integration test cases for NE EXE (#28)

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -6,31 +6,37 @@ import filecmp
 import os.path
 import unittest
 
+try:
+    import nefile
+except ImportError:
+    nefile = None
+
 import icoextract
 
-class IconExtractorTestCase(unittest.TestCase):
+class _BaseIconExtractorTestCase(unittest.TestCase):
+    def get_test_file(self, filename):
+        # Read/write test files in tests/ folder, regardless of where working directory is
+        tests_dir = os.path.dirname(__file__)
+        return os.path.join(tests_dir, filename)
+
     def _test_extract(self, infile, compare_against=None, **kwargs):
         """
         Wrapper to test extracting a single icon from infile, and comparing
         the output with an existing .ico file
         """
-        # Read/write test files in tests/ folder, regardless of where working directory is
-        tests_dir = os.path.dirname(__file__)
-        inpath = os.path.join(tests_dir, infile)
-
+        inpath = self.get_test_file(infile)
         ie = icoextract.IconExtractor(inpath)
-
-        outfile = f"tmp-{infile}.ico"
-        outpath = os.path.join(tests_dir, outfile)
+        outpath = self.get_test_file(f"tmp-extract-{infile}.ico")
         ie.export_icon(outpath, **kwargs)
 
         assert compare_against, \
             "Successful extractions should have a file to compare against"
-        compare_against = os.path.join(tests_dir, compare_against)
+        compare_against = self.get_test_file(compare_against)
         self.assertTrue(filecmp.cmp(outpath, compare_against),
                         f"{outpath} and {compare_against} should be equal")
         return ie
 
+class PEIconExtractorTestCase(_BaseIconExtractorTestCase):
     def test_basic(self):
         """Test basic extraction cases"""
         for app in ["testapp64.exe", "testapp32.exe"]:
@@ -45,8 +51,7 @@ class IconExtractorTestCase(unittest.TestCase):
         """Test list_group_icons() behaviour"""
         for app in ["testapp64.exe", "testapp32.exe"]:
             with self.subTest(app=app):
-                tests_dir = os.path.dirname(__file__)
-                inpath = os.path.join(tests_dir, app)
+                inpath = self.get_test_file(app)
                 ie = icoextract.IconExtractor(inpath)
 
                 icon_list = ie.list_group_icons()
@@ -62,7 +67,7 @@ class IconExtractorTestCase(unittest.TestCase):
                 ]
                 self.assertCountEqual(expected_sizes, real_sizes)
 
-    def test_no_icon_resource(self):
+    def test_no_icons(self):
         """Test that NoIconsAvailableError is raised when the input binary has
         no icons"""
         cases = [
@@ -113,6 +118,42 @@ class IconExtractorTestCase(unittest.TestCase):
         self.assertEqual(len(icon_list), 1)
         self.assertEqual(str(icon_list[0][0]), "MY_APP_ICON")
         self.assertGreater(int(icon_list[0][0]), 0)
+
+@unittest.skipUnless(nefile, "nefile library is not installed")
+class NEIconExtractorTestCase(_BaseIconExtractorTestCase):
+    def test_win16_basic(self):
+        """Test basic extraction cases"""
+        self._test_extract("testapp16.exe", "testapp-bpp.ico")
+        self._test_extract("testapp16.exe", "testapp-bpp.ico", resource_id="IDI_APPICON")
+
+        # Invalid IDs
+        with self.assertRaises(icoextract.IconNotFoundError):
+            self._test_extract("testapp64.exe", resource_id=1337)
+        with self.assertRaises(icoextract.IconNotFoundError):
+            self._test_extract("testapp64.exe", resource_id="FOO")
+
+    def test_win16_no_icons(self):
+        """Test programs with no icons"""
+        with self.assertRaises(icoextract.NoIconsAvailableError):
+            self._test_extract("testapp64-noicon.exe")
+        with self.assertRaises(icoextract.NoIconsAvailableError):
+            self._test_extract("testapp64-nores.exe")
+
+    def test_list(self):
+        """Test that list_group_icons() returns the right metadata"""
+        inpath = self.get_test_file("testapp16.exe")
+        ie = icoextract.IconExtractor(inpath)
+
+        icon_list = ie.list_group_icons()
+        self.assertEqual(len(icon_list), 1)
+        resource_id, grp_icons_with_offsets = icon_list[0]
+        self.assertEqual(str(resource_id), "IDI_APPICON")  # ID
+        self.assertEqual(len(grp_icons_with_offsets), 2)  # number of icons
+
+        for (grp_icons_dir_entry, _, _) in grp_icons_with_offsets:
+            self.assertEqual(grp_icons_dir_entry.Width, 32)
+            self.assertEqual(grp_icons_dir_entry.Height, 32)
+
 
 if __name__ == '__main__':
     unittest.main()
